19dd4077bcee465c6f9fb06d9d1b67d24fda1ee8
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2023-02-25 20:53:38 +0530

    Create PatientDonorDataService.java

---
+++ IKEP-microservices/patientdonordata/src/main/java/com/arghyabandyopadhyay/ikep/patientdonordata/service/PatientDonorDataService.java
@@ -0,0 +1,20 @@
+package com.arghyabandyopadhyay.ikep.patientdonordata.service;
+
+
+import com.arghyabandyopadhyay.ikep.patientdonordata.model.PatientDonorData;
+import com.arghyabandyopadhyay.ikep.patientdonordata.util.exception.PatientDonorDataAlreadyExistsException;
+import com.arghyabandyopadhyay.ikep.patientdonordata.util.exception.PatientDonorDataNotExistsException;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PatientDonorDataService {
+
+    public PatientDonorData addPatientDonorData(PatientDonorData patientDonorData) throws PatientDonorDataAlreadyExistsException;
+    public PatientDonorData getPatientDonorData(UUID id) throws PatientDonorDataNotExistsException;
+    public PatientDonorData getPatientDonorDataUsingMobileNumber(String mobileNo) throws PatientDonorDataNotExistsException;
+    public void updatePatientDonorData(PatientDonorData patientDonorData)throws PatientDonorDataNotExistsException;
+    public void deletePatientDonorData(UUID id)throws PatientDonorDataNotExistsException;
+
+    public List<PatientDonorData> getAllPatientDonorData();
+}
