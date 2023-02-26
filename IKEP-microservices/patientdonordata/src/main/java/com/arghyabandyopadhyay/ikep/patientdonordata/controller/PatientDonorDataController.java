b31b0ea90bc0fefe1e5ade1a5d23d497f1c9b3bb
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2023-02-26 13:52:37 +0530

    Update PatientDonorDataController.java

--- IKEP-microservices/patientdonordata/src/main/java/com/arghyabandyopadhyay/ikep/patientdonordata/controller/PatientDonorDataController.java
+++ IKEP-microservices/patientdonordata/src/main/java/com/arghyabandyopadhyay/ikep/patientdonordata/controller/PatientDonorDataController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -57,6 +59,40 @@ public class PatientDonorDataController {
             return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    @PostMapping("/csv")
+    public ResponseEntity<?> addPatientDonorDataUsingCSV(@RequestBody() String patientDonorDataCsv){
+        try {
+            String[] ar=patientDonorDataCsv.split(",");
+            PatientDonorData patientDonorData=new PatientDonorData(ar[0],ar[2], Arrays.asList(ar[3],ar[4],ar[5],ar[6],ar[7],ar[8]),Integer.parseInt(ar[9]),Double.parseDouble(ar[10]),Integer.parseInt(ar[11]),ar[12],ar[13], Arrays.asList(ar[14],ar[15],ar[16],ar[17],ar[18],ar[19]),Integer.parseInt(ar[20]),Double.parseDouble(ar[21]),Integer.parseInt(ar[22]),Integer.parseInt(ar[23]),Arrays.asList(ar[24].split(":")),ar[25],ar[1]);
+            PatientDonorData newPatientDonorData = patientDonorDataService.addPatientDonorData(patientDonorData);
+            return new ResponseEntity<>(newPatientDonorData,HttpStatus.CREATED);
+        } catch (PatientDonorDataAlreadyExistsException  e) {
+            return new ResponseEntity<String>("Patient-Donor Data already exists",HttpStatus.CONFLICT);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PostMapping("/csvmultiple")
+    public ResponseEntity<?> addPatientDonorDataUsingCSVMultiple(@RequestBody() String patientDonorDataCsvMultiple){
+        try {
+            String[] patientDonorDataCsvSplit=patientDonorDataCsvMultiple.split("\n");
+            List<PatientDonorData> patientDonorDataList=new ArrayList<>();
+            for(String patientDonorDataCsv : patientDonorDataCsvSplit){
+                String temp=patientDonorDataCsv.substring(0,patientDonorDataCsv.length()-1);
+                String[] ar=temp.split(",");
+                PatientDonorData patientDonorData=new PatientDonorData(ar[0],ar[2], Arrays.asList(ar[3],ar[4],ar[5],ar[6],ar[7],ar[8]),Integer.parseInt(ar[9]),Double.parseDouble(ar[10]),Integer.parseInt(ar[11]),ar[12],ar[13], Arrays.asList(ar[14],ar[15],ar[16],ar[17],ar[18],ar[19]),Integer.parseInt(ar[20]),Double.parseDouble(ar[21]),Integer.parseInt(ar[22]),Integer.parseInt(ar[23]),Arrays.asList(ar[24].split(":")),ar[25],ar[1]);
+                PatientDonorData newPatientDonorData = patientDonorDataService.addPatientDonorData(patientDonorData);
+                patientDonorDataList.add(newPatientDonorData);
+            }
+            return new ResponseEntity<>(patientDonorDataList,HttpStatus.CREATED);
+        } catch (PatientDonorDataAlreadyExistsException  e) {
+            return new ResponseEntity<String>("Patient-Donor Data already exists",HttpStatus.CONFLICT);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @PutMapping()
     public ResponseEntity<?> updateCustomerDetails(@RequestBody() PatientDonorData patientDonorData){
@@ -75,6 +111,19 @@ public class PatientDonorDataController {
     public ResponseEntity<?> deleteCustomerDetails(@RequestParam(name="identifier") UUID id){
         try {
             patientDonorDataService.deletePatientDonorData(id);
+            return new ResponseEntity<>("Patient-Donor Data deleted successfully",HttpStatus.OK);
+        } catch (PatientDonorDataNotExistsException e) {
+            return new ResponseEntity<>("Patient-Donor Data with specified details not found",HttpStatus.NOT_FOUND);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @DeleteMapping("/all")
+    public ResponseEntity<?> deleteCustomerDetails(@RequestParam(name="identifier") String id){
+        try {
+            if(id==null||!id.equals("yeSdeleteAllikep@*1234"))throw new PatientDonorDataNotExistsException();
+            patientDonorDataService.deleteAllPatientDonorData();
             return new ResponseEntity<>("Patient-Donor Data deleted successfully",HttpStatus.OK);
         } catch (PatientDonorDataNotExistsException e) {
             return new ResponseEntity<>("Patient-Donor Data with specified details not found",HttpStatus.NOT_FOUND);
